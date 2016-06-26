cafc6c774a825434d76ecddadc23927912bfa2b4
ElijahButers <Elijahbuters@gmail.com>
2016-06-26 11:35:23 +0300

    Updated code in login() that when the user taps the log in button and the status banner will appear with first progress message.

--- BahamaAir/BahamaAirLoginScreen/ViewController.swift
+++ BahamaAir/BahamaAirLoginScreen/ViewController.swift
@@ -123,7 +123,9 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     UIView.animateWithDuration(1.5, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 0.0, options: [], animations: {
         self.loginButton.bounds.size.width += 80.0
-        }, completion: nil)
+        }, completion: {_ in
+            self.showMessage(index: 0)
+    })
     UIView.animateWithDuration(0.33, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.0, options: [], animations: {
         self.loginButton.center.y += 60.0
         self.loginButton.backgroundColor = UIColor(red: 0.85, green: 0.83, blue: 0.45, alpha: 1.0)
